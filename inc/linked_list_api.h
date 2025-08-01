e572b34ca22e39bef9d7171e5718a5a6a634afbb
Anton <mosolov2000@gmail.com>
2025-08-01 10:12:22 +0300

    the iterator mechanism has been implemented

--- inc/linked_list_api.h
+++ inc/linked_list_api.h
@@ -12,11 +12,21 @@ typedef struct dns_packet_t  {
     struct dns_packet_t * next;
 }dns_packet_t;
 
+typedef struct iterator_t 
+{
+    dns_packet_t * packet;//pointer to the current packet in the linked list
+    dns_packet_t * head; //pointer to the beginning of the list, for cyclic iteration
+} iterator_t;
+
 
 
 void deleteList(void);
 void list_push(unsigned char *data, int length);
 dns_packet_t* get_dns_packet_list(void);
-void print_all_dns_packets(void);
+
+iterator_t * create_iterator(dns_packet_t *list);
+void delete_iterator(iterator_t * iter);
+unsigned char * get_dns_packet_from_iterator(iterator_t * iter);
+
 
 #endif
