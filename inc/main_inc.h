faa3077029f391dc30b16d618bc4ef75a895460a
Anton <mosolov2000@gmail.com>
2025-08-07 17:12:28 +0300

    the function to sending and receiving a packet has been rewritten to a packet implementation

--- inc/main_inc.h
+++ inc/main_inc.h
@@ -2,7 +2,7 @@
 #ifndef MAIN_INC_H
 #define MAIN_INC_H
 
-
+#define _GNU_SOURCE  
 
 #include <stdio.h>
 #include <stdlib.h>
--- src/dns_perf.c
+++ src/dns_perf.c
@@ -1,7 +1,7 @@
 
 #include "dns_perf_inc.h"
 #include "linked_list_api.h"
-
+#define NUMBER_OF_MESSAGE 100
 void *thread_body(void *arg)
 {
     struct thread_arg *args = (struct thread_arg *)arg;
@@ -14,10 +14,13 @@ void *thread_body(void *arg)
     printf("Thread started with IP: %s, Port: %d, P: %d, Working time: %d seconds\n", args->ip, args->port, args->P, args->working_time);
 
     int sockfd;
-    unsigned char buffer[MAX_UDP_MESSAGE_SIZE];
-    struct sockaddr_in servaddr, cliaddr;
+    unsigned char buffer[NUMBER_OF_MESSAGE][MAX_UDP_MESSAGE_SIZE];
+    struct sockaddr_in servaddr, cliaddr[NUMBER_OF_MESSAGE];
     int recv_len;
-    socklen_t client_address_len = sizeof(cliaddr);
+
+    struct mmsghdr msg[NUMBER_OF_MESSAGE]; // TODO change to calculated value k-packet
+    struct iovec iov[NUMBER_OF_MESSAGE];   // TODO change to calculated value k-packet
+
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
     {
         fprintf(stderr, "socket creation failed");
@@ -39,7 +42,6 @@ void *thread_body(void *arg)
         close(sockfd);
         return NULL;
     }
-
     memset(&servaddr, 0, sizeof(servaddr));
     memset(&cliaddr, 0, sizeof(cliaddr));
 
@@ -47,12 +49,45 @@ void *thread_body(void *arg)
     servaddr.sin_addr.s_addr = inet_addr(args->ip);
     servaddr.sin_port = htons(args->port);
 
-    dns_packet_t *dns_packet;
-    dns_packet = get_dns_packet_from_iterator(iter);
+    for (int i = 0; i < NUMBER_OF_MESSAGE; i++)
+    {
+        memset(&msg[i], 0, sizeof(struct mmsghdr));
+        memset(&iov[i], 0, sizeof(struct iovec));
+        dns_packet_t *dns_packet = get_dns_packet_from_iterator(iter);
+        iov[i].iov_base = dns_packet->packet;
+        iov[i].iov_len = dns_packet->packet_length;
+        msg[i].msg_hdr.msg_iov = &iov[i];
+        msg[i].msg_hdr.msg_name = &servaddr;
+        msg[i].msg_hdr.msg_namelen = sizeof(servaddr);
+        msg[i].msg_hdr.msg_iovlen = 1;
+        msg[i].msg_hdr.msg_control = NULL;
+        msg[i].msg_hdr.msg_controllen = 0;
+        msg[i].msg_hdr.msg_flags = 0;
+    }
+    int sent = sendmmsg(sockfd, msg, NUMBER_OF_MESSAGE, 0);
 
-    sendto(sockfd, dns_packet->packet, dns_packet->packet_length, MSG_CONFIRM, (struct sockaddr *)&servaddr, sizeof(servaddr));
-    memset(&buffer, 0, sizeof(buffer));
-    recv_len = recvfrom(sockfd, buffer, MAX_UDP_MESSAGE_SIZE - 1, 0, (struct sockaddr *)&cliaddr, &client_address_len);
+    for (int i = 0; i < NUMBER_OF_MESSAGE; i++)
+    {
+        memset(&msg[i], 0, sizeof(struct mmsghdr));
+        memset(&iov[i], 0, sizeof(struct iovec));
+        iov[i].iov_base = buffer[i];
+        iov[i].iov_len = MAX_UDP_MESSAGE_SIZE;
+        msg[i].msg_hdr.msg_iov = &iov[i];
+        msg[i].msg_hdr.msg_name = &cliaddr[i];
+        msg[i].msg_hdr.msg_namelen = sizeof(struct sockaddr_in);
+        msg[i].msg_hdr.msg_iovlen = 1;
+        msg[i].msg_hdr.msg_control = NULL;
+        msg[i].msg_hdr.msg_controllen = 0;
+        msg[i].msg_hdr.msg_flags = 0;
+    }
+
+    int received = recvmmsg(sockfd, msg, NUMBER_OF_MESSAGE, 0, NULL);
+    if (received < 0)
+    {
+        fprintf(stderr, "recvmmsg failed \n");
+        close(sockfd);
+        
+    }
 
     close(sockfd);
     delete_iterator(iter); // deleting the iterator
